a91d31e3a601e0869139d6fd8ced61bd389be1db
Kian Sharifi <kiansharifi88@gmail.com>
2023-09-07 22:47:50 -0700

    Initialize method

--- Index.java
+++ Index.java
@@ -1,4 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Index
 {
+    public static void initialize () throws IOException
+    {
+        File yourFile = new File("index");
+        yourFile.createNewFile(); // if file already exists will do nothing
+        Files.createDirectories(Paths.get("objects/"));
+    }
 
+    public static void addBlob (String TestFile) throws IOException
+    {
+        Blob blob = new Blob (TestFile);
+        File f = new File(TestFile);
+        String name = f.getName ();
+    }
 }
--- Tester.java
+++ Tester.java
@@ -5,5 +5,7 @@ public class Tester
     public static void main (String [] args) throws IOException
     {
         Blob blob = new Blob ("TestFile");
+        Index.initialize ();
     }
+
 }
