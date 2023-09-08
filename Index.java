b19db649027d4f0d3913d98a82f78c03b788652a
Kian Sharifi <kiansharifi88@gmail.com>
2023-09-08 16:24:32 -0700

    Finished remove

--- Index.java
+++ Index.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -7,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Index {
     public static void initialize() throws IOException {
@@ -27,22 +29,49 @@ public class Index {
     }
 
     public static void addBlob(String TestFile) throws IOException {
-        initialize ();
+        initialize();
         Blob blob = new Blob(TestFile);
         File f = new File(TestFile);
         File indexFile = new File("index");
         String name = f.getName();
-        if (reader ("index").contains (blob.getSha1 ()))
-        {
+        if (reader("index").contains(blob.getSha1())) {
             return;
         }
         FileWriter writer = new FileWriter(indexFile, true);
-        if (!reader ("index").isEmpty ())
-        {
-            writer.write ("\n");
+        if (!reader("index").isEmpty()) {
+            writer.write("\n");
         }
         writer.write(name + " : " + blob.getSha1());
         writer.close();
+    }
+
+    public static void removeBlob(String TestFile) throws IOException {
+
+        File f = new File(TestFile);
+        String name = f.getName();
+        String file = reader("index");
+        if (!file.contains(name)) {
+            return;
+        }
+        File inputFile = new File("index");
+        File tempFile = new File("TempFile");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String lineToRemove = name;
+        String currentLine;
+
+        while ((currentLine = reader.readLine()) != null) {
+            // trim newline when comparing with lineToRemove
+            String trimmedLine = currentLine.trim();
+            if (trimmedLine.split(" : ")[0].equals((lineToRemove)))
+                continue;
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+        writer.close();
+        reader.close();
+        tempFile.renameTo(inputFile);
     }
 
 }
--- Tester.java
+++ Tester.java
@@ -8,6 +8,8 @@ public class Tester
         Index.initialize ();
         Index.addBlob ("TestFile");
         Index.addBlob ("TestFile2");
+        Index.addBlob ("TestFile3");
+        Index.removeBlob ("TestFile");
     }
 
 }
