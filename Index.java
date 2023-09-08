018e79429f394abdd520d2c7f4ea0ad1fc56a466
Kian Sharifi <kiansharifi88@gmail.com>
2023-09-07 22:57:02 -0700

    addBlob method

--- Index.java
+++ Index.java
@@ -1,23 +1,40 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Index
-{
-    public static void initialize () throws IOException
-    {
-        File yourFile = new File("index");
-        yourFile.createNewFile(); // if file already exists will do nothing
+public class Index {
+    public static void initialize() throws IOException {
+        File indexFile = new File("index");
+        indexFile.createNewFile(); // if file already exists will do nothing
         Files.createDirectories(Paths.get("objects/"));
     }
 
-    public static void addBlob (String TestFile) throws IOException
-    {
-        Blob blob = new Blob (TestFile);
-        File f = new File(TestFile);
-        String name = f.getName ();
+    public String reader(String TestFile) throws IOException {
+        StringBuilder output = new StringBuilder();
+        BufferedReader breader = new BufferedReader(new FileReader(TestFile));
+        while (breader.ready()) {
+            String s = breader.readLine();
+            output.append(s);
+        }
+        breader.close();
+        return output.toString();
     }
+
+    public static void addBlob(String TestFile) throws IOException {
+        initialize ();
+        Blob blob = new Blob(TestFile);
+        File f = new File(TestFile);
+        File indexFile = new File("index");
+        String name = f.getName();
+        FileWriter writer = new FileWriter(indexFile, true);
+        writer.write(name + " : " + blob.getSha1());
+        writer.close();
+    }
+
 }
