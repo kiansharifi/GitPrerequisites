59220804bff1954f8a0ece26668ed37eb990e060
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-11 14:44:08 -0700

    Utils

---
+++ Utils.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Utils {
+    public static void writeStringToFile(String path, String contents) throws IOException {
+        FileWriter fw = new FileWriter(path, false);
+        fw.write(contents);
+        fw.close();
+    }
+
+    public static void deleteFile(String path) {
+        File file = new File(path);
+        file.delete();
+    }
+
+    public static void deleteDirectory(String path) {
+        File file = new File(path);
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                if (!Files.isSymbolicLink(f.toPath())) {
+                    deleteDirectory(f.getPath());
+                }
+            }
+        }
+        file.delete();
+    }
+}
