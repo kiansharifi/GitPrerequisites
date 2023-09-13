daab14b7064659cdd2a549cd15691339f8f2b7cb
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-13 10:39:35 -0700

    Tree methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,12 @@
 package src;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class Tree {
@@ -11,10 +17,10 @@ public class Tree {
         return fileContents;
     }
 
-    public void addEntry(String contents) {
+    public void add(String contents) {
         String[] parts = contents.split(" : ");
         String shaOfFile = parts[1];
-        String optionalFileName = parts.length > 1 ? parts[2] : "";
+        String optionalFileName = parts.length > 2 ? parts[2] : "";
 
         if (!fileContents.contains(shaOfFile)
                 && (optionalFileName.isEmpty() || (!fileContents.contains(optionalFileName)))) {
@@ -22,7 +28,7 @@ public class Tree {
         }
     }
 
-    public void removeEntry(String entry) {
+    public void remove(String entry) {
         String newFileContents = "";
 
         Scanner inContents = new Scanner(fileContents);
@@ -37,6 +43,40 @@ public class Tree {
         inContents.close();
 
         fileContents = newFileContents;
+    }
+
+    public void save() throws IOException {
+        fileContents = fileContents.stripTrailing();
+        File file = new File("objects/" + getSha1(fileContents));
+        FileWriter fw = new FileWriter(file);
+        fw.write(fileContents);
+        fw.close();
+    }
+
+    public String getSha1(String input) throws IOException {
+        String passwordString = input;
+        String sha1 = "";
+        try {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(passwordString.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    public String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 
 }
