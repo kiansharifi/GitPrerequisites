4f66498c0cce23602ff285db64794922f1198b66
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-13 10:26:08 -0700

    Tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,5 +1,42 @@
 package src;
 
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Tree {
+
+    String fileContents = "";
+
+    public String getFileContents() {
+        return fileContents;
+    }
+
+    public void addEntry(String contents) {
+        String[] parts = contents.split(" : ");
+        String shaOfFile = parts[1];
+        String optionalFileName = parts.length > 1 ? parts[2] : "";
+
+        if (!fileContents.contains(shaOfFile)
+                && (optionalFileName.isEmpty() || (!fileContents.contains(optionalFileName)))) {
+            fileContents += contents + "\n";
+        }
+    }
+
+    public void removeEntry(String entry) {
+        String newFileContents = "";
+
+        Scanner inContents = new Scanner(fileContents);
+
+        while (inContents.hasNextLine()) {
+            String line = inContents.nextLine();
+            if (!line.contains(entry)) {
+                newFileContents += line + "\n";
+            }
+        }
+
+        inContents.close();
+
+        fileContents = newFileContents;
+    }
 
 }
