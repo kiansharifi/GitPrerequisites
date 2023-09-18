20c1203e98c91e16355d753e33f6beda4f3e3aa6
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-18 15:43:10 -0700

    Test changes

--- tests/TreeTest.java
+++ tests/TreeTest.java
@@ -34,9 +34,9 @@ public class TreeTest {
     @Test
     void testAdd() {
         Tree tree = new Tree();
-        tree.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+        tree.add("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f : file1.txt");
 
-        assertEquals("Incorrect file contents", "tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b",
+        assertEquals("Incorrect file contents", "blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f : file1.txt",
                 tree.getFileContents());
     }
 
@@ -76,8 +76,8 @@ public class TreeTest {
     @Test
     void testRemove() {
         Tree tree = new Tree();
-        tree.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
-        tree.remove("bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+        tree.add("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f : file1.txt");
+        tree.remove("file1.txt");
 
         assertEquals("Incorrect file contents", "", tree.getFileContents());
     }
