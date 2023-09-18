5e6806afaf0d9349d54276f9960a2beed62f0f2d
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-18 15:19:22 -0700

    Blob tester

--- tests/BlobTest.java
+++ tests/BlobTest.java
@@ -1,14 +1,20 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import src.Git.Blob;
+import src.Git.Index;
 import src.Git.TestUtils;
 
 public class BlobTest {
@@ -31,27 +37,60 @@ public class BlobTest {
         Blob blob = new Blob("test_file");
 
         byte[] empty = {};
-        assertEquals("Correct behavior for empty array", "", blob.byteToHex(empty));
+        assertEquals("Incorrect behavior for empty array", "", blob.byteToHex(empty));
 
+        byte[] nonEmpty = { 0x00, 0x01, 0x02, 0x03 };
+        assertEquals("Incorrect behavior for non-empty array", "00010203", blob.byteToHex(nonEmpty));
     }
 
     @Test
-    void testEncryptPassword() {
+    void testEncryptPassword() throws IOException {
+        // SHA1 of "test file contents" is cbaedccfded0c768295aae27c8e5b3a0025ef340
+        Blob blob = new Blob("test_file");
 
+        assertEquals("SHA1 hash of file not correct", blob.encryptPassword("test_file"),
+                "cbaedccfded0c768295aae27c8e5b3a0025ef340");
     }
 
     @Test
-    void testGetSha1() {
+    void testGetSha1() throws IOException {
+        Blob blob = new Blob("test_file");
 
+        blob.encryptPassword("test_file");
+
+        assertEquals("getSha1() doesn't return last hash of file", blob.getSha1(),
+                "cbaedccfded0c768295aae27c8e5b3a0025ef340");
     }
 
     @Test
-    void testReader() {
+    void testReader() throws IOException {
+        Blob blob = new Blob("test_file");
+        String file_contents = blob.reader("test_file");
 
+        assertEquals("Reader does not return contents of file", file_contents, "test file contents");
     }
 
     @Test
-    void testWriteToObjects() {
+    void testWriteToObjects() throws IOException {
+        String sha1 = "";
 
+        Index.addBlob("test_file");
+        Blob blob = new Blob("test_file");
+        sha1 = blob.getSha1();
+
+        // Check blob exists in the objects folder
+        Path path = Paths.get("objects/" + sha1);
+        assertTrue("Blob file to add not found", Files.exists(path));
+
+        File index = new File("index");
+
+        // Read file contents
+        String indexFileContents = "test_file : " + sha1;
+        assertEquals("File contents of index not updated", indexFileContents,
+                Files.readString(Path.of(index.getPath())));
+
+        String originalFileContents = blob.reader("test_file");
+        assertEquals("File contents of Blob don't match file contents pre-blob creation", originalFileContents,
+                Files.readString(Path.of(path.toString())));
     }
 }
